f8d455a679f4ae876c556407308d0c8bb6f3125f
Taiga Yoshizaki <tiger.33255@gmail.com>
2019-07-24 08:15:53 +0000

    Update Gemfile for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_07_055315) do
+ActiveRecord::Schema.define(version: 2019_07_24_080033) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2019_07_07_055315) do
     t.string "title"
     t.string "body"
     t.string "image_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
