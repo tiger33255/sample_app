a12547fe0cc6dc3c21514aac7bf98edc501be341
Taiga Yoshizaki <tiger.33255@gmail.com>
2019-07-24 09:31:48 +0000

    Finish toy app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_080033) do
+ActiveRecord::Schema.define(version: 2019_07_24_084705) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
